3dbbb5be84d668dc2bd23728d029433ecff29f81
ishantgupta1608 <ishantgupta1608@gmail.com>
2020-03-24 16:05:35 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    #file = open('Timestamp.txt', 'a')
-    #file.write(str(datetime.now()) + '\n')
+    file = open('Timestamp.txt', 'a')
+    file.write(str(datetime.now()) + '\n')
     return "Hello World!!!!!"
 
 
 if __name__ == "__main__":
     #print(2)
-    app.run(debug = True)
+    app.run()
